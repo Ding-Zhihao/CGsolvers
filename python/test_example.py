c399c38cb0db68b8a74437ea74dbf80024f3bb54
Ding-Zhihao <tomding0107@gmail.com>
2023-07-01 03:07:56 +0800

    Python version done, without preconditioning.

---
+++ python/test_example.py
@@ -0,0 +1,50 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+A = np.loadtxt("../data/Amatrix.txt")
+b = np.loadtxt("../data/bc_vector.txt").reshape(-1, 1)
+x_ref = np.loadtxt("../data/Result.txt").reshape(-1, 1)
+n = len(x_ref)
+
+converged = False
+x = np.zeros((n, 1))
+r = b - np.dot(A, x)
+r0_hat = r * 1
+rho0 = 1.0
+alpha = 1.0
+w = 1.0
+p = np.zeros((n, 1))
+v = np.zeros((n, 1))
+
+tol = 1.0e-100
+limit = 100
+iters = 0
+
+while iters < limit:
+    iters = iters + 1
+    tol_current = np.linalg.norm(r) / np.linalg.norm(b)
+    converged = tol_current < tol
+    if converged == True:
+        break
+    rho1 = np.dot(np.transpose(r0_hat), r)
+    beta = rho1 / rho0 * alpha / w
+    p = r + beta * (p - w * v)
+    v = np.dot(A, p)
+    alpha = rho1 / np.dot(np.transpose(r0_hat), v)
+    s = r - alpha * v
+    t = np.dot(A, s)
+    w = np.dot(np.transpose(t), s) / np.dot(np.transpose(t), t)
+    rho0 = rho1
+    x += alpha * p + w * s
+    r = s - w * t
+
+print("number of iterations", iters)
+print("residual", tol_current)
+
+plt.figure()
+plt.scatter(x_ref, x)
+plt.plot([0, 1], [0, 1], "k--")
+plt.xlabel("Jacobi")
+plt.ylabel("BiCGStab")
+plt.grid()
---
+++ python/test_scipy.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy.sparse import csc_matrix
+from scipy.sparse.linalg import bicgstab
+import matplotlib.pyplot as plt
+
+# load data
+R = np.loadtxt("../data/Amatrix.txt")
+A = csc_matrix(R)
+b = np.loadtxt("../data/bc_vector.txt")
+x_ref = np.loadtxt("../data/Result.txt")
+
+# computation
+x, exit_code = bicgstab(A, b, tol=1e-100)
+
+# post
+plt.figure()
+plt.scatter(x_ref, x)
+plt.plot([0, 1], [0, 1], "k--")
+plt.xlabel("Jacobi")
+plt.ylabel("BiCGStab")
+plt.grid()
