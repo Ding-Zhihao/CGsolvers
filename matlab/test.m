6a2ffc990eac296cd870c681a936745088cd1e54
Ding-Zhihao <tomding0107@gmail.com>
2023-07-01 22:32:56 +0800

    add iLU test

--- matlab/test.m
+++ matlab/test.m
@@ -4,8 +4,14 @@ clc
 
 A = readmatrix('../data/Amatrix.txt');
 b = readmatrix('../data/bc_vector.txt');
-x = bicgstab(A,b, 1e-8);
 x_ref = readmatrix('../data/Result.txt');
+
+A=sparse(A);
+
+tol=1e-20;
+maxit=100;
+
+[x,fl0,rr0,it0,rv0] = bicgstab(A,b, tol, maxit);
 
 figure,
 scatter(x, x_ref)
@@ -14,3 +20,26 @@ plot([0,1], [0,1], 'k')
 xlabel("bicgstab")
 ylabel("Jacobi")
 grid on
+hold off
+
+options = struct("type","ilutp","droptol",1e-6);
+[L,U] = ilu(A,options);
+[x_precond,fl1,rr1,it1,rv1] = bicgstab(A,b,tol,maxit,L,U);
+
+figure,
+scatter(x_precond, x_ref)
+hold on
+plot([0,1], [0,1], 'k')
+xlabel("bicgstab + iLU")
+ylabel("Jacobi")
+grid on
+hold off
+
+figure,
+semilogy((0:numel(rv0)-1)/2,rv0/norm(b),"-o")
+hold on
+semilogy((0:numel(rv1)-1)/2,rv1/norm(b),"-o")
+yline(tol,"r--");
+legend("No preconditioner","ILU preconditioner","Tolerance",Location="East")
+xlabel("Iteration number")
+ylabel("Relative residual")
--- python/test_scipy.py
+++ python/test_scipy.py
@@ -2,6 +2,19 @@ import numpy as np
 from scipy.sparse import csc_matrix
 from scipy.sparse.linalg import bicgstab
 import matplotlib.pyplot as plt
+from scipy import sparse
+from scipy.sparse import linalg
+
+class bicgstab_counter(object):
+    def __init__(self, disp=True):
+        self._disp = disp
+        self.niter = 0
+    def __call__(self, xk=None):
+        self.niter += 1
+        if self._disp:
+            print('iter %3i' % (self.niter))
+
+
 
 # load data
 R = np.loadtxt("../data/Amatrix.txt")
@@ -10,7 +23,8 @@ b = np.loadtxt("../data/bc_vector.txt")
 x_ref = np.loadtxt("../data/Result.txt")
 
 # computation
-x, exit_code = bicgstab(A, b, tol=1e-100)
+counter1 = bicgstab_counter()
+x, exit_code = bicgstab(A, b, tol=1e-20, callback=counter1)
 
 # post
 plt.figure()
@@ -19,3 +33,19 @@ plt.plot([0, 1], [0, 1], "k--")
 plt.xlabel("Jacobi")
 plt.ylabel("BiCGStab")
 plt.grid()
+
+counter2 = bicgstab_counter()
+sA = sparse.csc_matrix(A)
+sA_iLU = sparse.linalg.spilu(sA)
+M = sparse.linalg.LinearOperator((918, 918), sA_iLU.solve)
+x_precond, exit_code = bicgstab(A, b, tol=1e-20, M=M, callback=counter2)
+
+# post
+plt.figure()
+plt.scatter(x_ref, x_precond)
+plt.plot([0, 1], [0, 1], "k--")
+plt.xlabel("Jacobi")
+plt.ylabel("BiCGStab iLU")
+plt.grid()
+
+
