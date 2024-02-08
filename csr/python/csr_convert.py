e4ec4f3a2c1a4db59c39398d811f7bf84532c92e
Ding-Zhihao <tomding0107@gmail.com>
2024-02-08 17:23:43 +0800

    doing matrix multiplication

--- csr/python/csr_convert.py
+++ csr/python/csr_convert.py
@@ -20,30 +20,32 @@ class csr_matrix_:
         shape = self.shape
         nnz = self.nnz
 
-        counter = np.zeros(shape[1], dtype='int32')
+        counter = np.zeros(shape[1], dtype="int32")
         for k in range(nnz):
             counter[indices[k]] += 1
 
-        new_indptr = np.zeros(shape[1]+1, dtype='int32')
+        new_indptr = np.zeros(shape[1] + 1, dtype="int32")
         for i in range(1, len(new_indptr)):
-            new_indptr[i]=np.sum(counter[:i])
-            
-        
+            new_indptr[i] = np.sum(counter[:i])
+
         new_indices = np.zeros(nnz)
         new_data = np.zeros(nnz)
         for i in range(shape[0]):
-            for j in range(indptr[i], indptr[i+1]):
+            for j in range(indptr[i], indptr[i + 1]):
                 index = int(new_indptr[indices[j]])
                 new_data[index] = data[j]
                 new_indices[index] = i
-                new_indptr[indices[j]]+=1
-        
-        new_indptr2=np.zeros(shape[1]+1, dtype='int32')
-        new_indptr2[1:]=new_indptr[:-1]
-        shape=(shape[1], shape[0])
-        
-        tr = csr_matrix_(np.array(new_data), np.array(new_indices), np.array(new_indptr2), shape)
+                new_indptr[indices[j]] += 1
+
+        new_indptr2 = np.zeros(shape[1] + 1, dtype="int32")
+        new_indptr2[1:] = new_indptr[:-1]
+        shape = (shape[1], shape[0])
+
+        tr = csr_matrix_(
+            np.array(new_data), np.array(new_indices), np.array(new_indptr2), shape
+        )
         return tr
+
 
 def dense_to_csr(A):
     nnz = 0
@@ -63,23 +65,19 @@ def dense_to_csr(A):
 
 
 if __name__ == "__main__":
-    A = loadmat("../data/input")["A"]
-    # A=np.random.rand(1000,1000)
+    rng = np.random.default_rng()
+    A = sparse.random(1000, 1000, density=0.1, random_state=rng).toarray()
     A_csr_ref = sparse.csr_matrix(A)
     A_csr_tr_ref = sparse.csr_matrix(A.transpose())
     A_csr = dense_to_csr(A)
     A_csr_tr = A_csr.transpose()
-    
 
     assert np.array_equal(A_csr.data, A_csr_ref.data)
     assert np.array_equal(A_csr.indices, A_csr_ref.indices)
     assert np.array_equal(A_csr.indptr, A_csr_ref.indptr)
     assert np.array_equal(A_csr.shape, A_csr_ref.shape)
-    
+
     assert np.array_equal(A_csr_tr.data, A_csr_tr_ref.data)
     assert np.array_equal(A_csr_tr.indices, A_csr_tr_ref.indices)
     assert np.array_equal(A_csr_tr.indptr, A_csr_tr_ref.indptr)
     assert np.array_equal(A_csr_tr.shape, A_csr_tr_ref.shape)
-
-
-    
--- csr/python/csr_operation.py
+++ csr/python/csr_operation.py
@@ -5,6 +5,62 @@ from scipy import sparse
 from scipy.io import loadmat, savemat
 from csr_convert import *
 
-A = loadmat('../data/input')['A']
-A_csr = to_csr(A)
 
+def get_csr_row(A_csr, row):
+    data = A_csr.data[A_csr.indptr[row] : A_csr.indptr[row + 1]]
+    indices = A_csr.indices[A_csr.indptr[row] : A_csr.indptr[row + 1]]
+    indptr = A_csr.indptr[row : row + 2]
+    indptr = indptr - indptr[0]
+    shape = (1, A_csr.shape[1])
+    row_csr = csr_matrix_(data, indices, indptr, shape)
+    return row_csr
+
+
+if __name__ == "__main__":
+    rng = np.random.default_rng()
+    A = sparse.random(100, 50, density=0.01, random_state=rng)
+    B = sparse.random(50, 100, density=0.01, random_state=rng)
+    out_ref = A * B
+    A_csr = dense_to_csr(A.toarray())
+    B_csr = dense_to_csr(B.toarray())
+
+    row_data_ref = sparse.csr_matrix(A.toarray()[0, :])
+    row_data = get_csr_row(A_csr, 0)
+
+    assert np.array_equal(row_data.data, row_data_ref.data)
+    assert np.array_equal(row_data.indices, row_data_ref.indices)
+    assert np.array_equal(row_data.indptr, row_data_ref.indptr)
+    assert np.array_equal(row_data.shape, row_data_ref.shape)
+
+    B_csr_tr = B_csr.transpose()
+    
+    
+    nnz = 0
+    data = []
+    indices = []
+    indptr = [0]
+    for i in range(A_csr.shape[0]):
+        indptr.append(indptr[i])
+        row_A = get_csr_row(A_csr, i)
+        if row_A.nnz > 0:
+            for j in range(B_csr_tr.shape[0]):
+                row_B = get_csr_row(B_csr_tr, j)
+                if row_B.nnz > 0:
+                    sum = 0.0
+                    for k in range(row_A.nnz):
+                        for m in range(row_B.nnz):
+                            if row_A.indices[k] == row_B.indices[m]:
+                                sum+=row_A.data[k]*row_B.data[m]
+                    
+                    if sum != 0.0:
+                        data.append(sum)
+                        indices.append(j)
+                        indptr[i + 1] += 1
+                        nnz += 1
+                        
+    out_csr = csr_matrix_(np.array(data), np.array(indices), np.array(indptr), (A_csr.shape[0], B_csr_tr.shape[0]))
+
+    assert np.array_equal(out_ref.data, out_csr.data)
+    assert np.array_equal(out_ref.indices, out_csr.indices)
+    assert np.array_equal(out_ref.indptr, out_csr.indptr)
+    assert np.array_equal(out_ref.shape, out_csr.shape)
